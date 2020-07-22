d4dec66ce04d22d0a1b1c7268feb96d9d3a4b5db
Vadim <50245404+Zitronius@users.noreply.github.com>
2020-07-22 21:32:00 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.promo').slick({
+        arrows: false,
+        dots: true,
+    });
+    $('.popular__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+});
